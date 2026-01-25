17a5250d09f1c52f99c54dc6208c58fa7a97f728
Laurie Hess <lettergriddle@gmail.com>
2026-01-24 22:24:57 -0500

    Add custom Home Screen icon for Letter Griddle

--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,24 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css'
 
 export const metadata = {
-  title: 'Letter Griddle Game - Daily Word Puzzle Game 🥞  ',
+  title: 'Letter Griddle Game - Daily Word Puzzle Game 🥞',
   description: 'Stack letters like pancakes in this delicious daily word puzzle game!',
+  icons: {
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body>
         {children}
         <Analytics />
         <SpeedInsights />
-        </body>
+      </body>
     </html>
   )
 }

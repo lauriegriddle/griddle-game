0a11ff1572fe6c09c097cecde285f31bbd081a0b
Laurie Hess <lettergriddle@gmail.com>
2026-03-31 10:57:18 -0400

    Add Cafe Synth at /synth

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/synth',
+        destination: '/synth.html',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
